3623d62f9e0e2f2e337280d286e23d74b88312a7
kyri04 <k.kyriacou04@gmail.com>
2025-10-06 14:57:23 +0100

    more xp/np changes

--- learning.py
+++ learning.py
@@ -29,7 +29,7 @@ def classify(
 
     labels_encoded, encoder = encode(labels)
     unique_labels = encoder.classes_
-    unique_labels_encoded = xp.unique(labels_encoded)
+    unique_labels_encoded = np.unique(labels_encoded)
 
     train_idx, test_idx = get_single_split(N, labels_encoded, groups, test_size, split_state)
 
